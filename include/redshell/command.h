a936786d8cb72c49aedc549fcb1ccd4fc4c6bb45
Timothy Mead <t.mead@cyborgdynamicseng.com>
2024-07-05 20:40:55 +1000

    Update command to use signed speed type

--- include/redshell/command.h
+++ include/redshell/command.h
@@ -5,7 +5,7 @@
 
 #define REDSHELL_MSG_ID_COMMAND 0x2
 
-PacketInfo msg_command_encode(uint32_t speed_left_pct, uint32_t speed_right_pct) {
+PacketInfo msg_command_encode(int32_t speed_left_pct, int32_t speed_right_pct) {
     PacketInfo result;
     result.start = REDSHELL_START_BYTE;
     result.id = REDSHELL_MSG_ID_COMMAND;
@@ -22,7 +22,7 @@ PacketInfo msg_command_encode(uint32_t speed_left_pct, uint32_t speed_right_pct)
     return result;
 }
 
-void msg_command_decode(PacketInfo packet, uint32_t* speed_left_pct, uint32_t* speed_right_pct) {
+void msg_command_decode(PacketInfo packet, int32_t* speed_left_pct, int32_t* speed_right_pct) {
     *speed_left_pct = packet.data[0] | (packet.data[1] << 8) | (packet.data[2] << 16) | (packet.data[3] << 24);
     *speed_right_pct = packet.data[4] | (packet.data[5] << 8) | (packet.data[6] << 16) | (packet.data[7] << 24);
 }
